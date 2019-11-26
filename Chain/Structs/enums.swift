b5a384c79a6aa55a3547451fd1c6c723833cff15
Christian <crburke3@ncsu.edu>
2019-11-26 15:01:28 -0500

    Firebase Setup

---
+++ Chain/Structs/enums.swift
@@ -0,0 +1,15 @@
+//
+//  enums.swift
+//  Chain
+//
+//  Created by Christian Burke on 11/26/19.
+//  Copyright © 2019 Christian Burke. All rights reserved.
+//
+
+import Foundation
+
+//Used as easy ways to save Strings and stuff to the phone
+enum SaveKeys:String{
+    case username
+    case password
+}
