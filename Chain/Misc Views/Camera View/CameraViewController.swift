80156b955f13b5934ff36612513c89886ce37997
Christian <crburke3@ncsu.edu>
2020-01-05 15:08:13 -0500

    Image editor works!

--- Chain/Misc Views/Camera View/CameraViewController.swift
+++ Chain/Misc Views/Camera View/CameraViewController.swift
@@ -12,14 +12,15 @@ import Kingfisher
 import Firebase
 import iOSPhotoEditor
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, PhotoEditorDelegate {
+
+    
 
     @IBOutlet var previewView: CKFPreviewView!
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var cameraButton: UIButton!
     let session = CKFPhotoSession()
     var chainID: String = ""
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,18 +29,11 @@ class CameraViewController: UIViewController {
         previewView.session = session
         cameraButton.isHidden = false
         view.bringSubviewToFront(cameraButton)
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func cameraTouched(_ sender: Any) { //Create Chain Image
-        //Capture Image
         captureImage()
-        //Confirm Image, if denied then take another image
-        
-        //Once confirmed, upload to FireBase and then append to chain
-        
     }
-    
     
     @IBAction func cameraHeldDown(_ sender: Any) {
         print("held down")
@@ -56,35 +50,29 @@ class CameraViewController: UIViewController {
             self.imageView.isHidden = false
             print(self.imageView.frame)
             self.cameraButton.isHidden = true
-            //Present confirm or denie buttons
-            
-                let photoEditor = UIStoryboard(name: "Main", bundle: Bundle(for: PhotoEditorViewController.self)).instantiateViewController(withIdentifier: "PhotoEditorViewController") as! PhotoEditorViewController
-
-                //PhotoEditorDelegate
-                photoEditor.photoEditorDelegate = self as! PhotoEditorDelegate
-
-                //The image to be edited
-                photoEditor.image = image
-
-                //Stickers that the user will choose from to add on the image
-                photoEditor.stickers.append(UIImage(named: "sticker" )!)
-
-                //To hide controls - array of enum control
-                photoEditor.hiddenControls = [.crop, .draw, .share]
-
-                //Present the View Controller
-                self.present(photoEditor, animated: true, completion: nil)
-            
-            masterFire.appendChain(chainID: self.chainID, image: self.imageView.image!) { (error) in
-                if let error = error {
-                    print("Error appending to chain \(error)")
-                } else {
-                    
-                }
-            }
-            
+            let photoEditor = PhotoEditorViewController(nibName:"PhotoEditorViewController",bundle: Bundle(for: PhotoEditorViewController.self))
+            photoEditor.photoEditorDelegate = self
+            photoEditor.image = image
+            //photoEditor.stickers.append(UIImage(named: "sticker" )!)
+            photoEditor.hiddenControls = [.share]
+            photoEditor.modalPresentationStyle = UIModalPresentationStyle.currentContext //or .overFullScreen for transparency
+            self.present(photoEditor, animated: true, completion: nil)
         }) { (error) in
            
         }
+    }
+    
+    func doneEditing(image: UIImage) {
+        masterFire.appendChain(chainID: self.chainID, image: self.imageView.image!) { (error) in
+            if let error = error {
+                print("Error appending to chain \(error)")
+            } else {
+
+            }
+        }
+    }
+    
+    func canceledEditing() {
+        
     }
 }
